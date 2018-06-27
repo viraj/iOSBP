877dd110811cfa48eb0b894af30cbb48f07d1881
Viraj Thenuwara <vthenuwara@gmail.com>
2018-06-27 17:52:01 +0530

    initial commit

---
+++ Pods/Target Support Files/Pods-iOSBP/Pods-iOSBP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSBP : NSObject
+@end
+@implementation PodsDummy_Pods_iOSBP
+@end
---
+++ iOSBP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOSBP
+//
+//  Created by Viraj Thenuwara on 6/27/18.
+//  Copyright © 2018 Viraj Thenuwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        self.view.accessibilityLabel = "home_screen"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
